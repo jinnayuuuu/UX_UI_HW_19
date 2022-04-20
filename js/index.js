cf6db697b6965f66ad241c84ebb04a343d80a6de
jinnayuuuu <jinnayu@utexas.edu>
2022-04-19 23:33:46 -0500

    update~

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 console.log("Hello world!");
+
+$("download resume").click(function(){
+    alert("Oops, looks like I haven't uploaded it yet.");
+  });
+
+const name = document.querySelector(".name");
+function changeColor() {
+    name.style.color = "blue";
+}
